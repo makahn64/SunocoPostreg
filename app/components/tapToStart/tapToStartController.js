54ea0ed6acc5441a614d103787d53d5976c3be67
atorres <torres.lyssa@gmail.com>
2017-02-03 15:53:28 -0800

    access setup page fixes

---
+++ app/components/tapToStart/tapToStartController.js
@@ -0,0 +1,26 @@
+app.controller("tapToStartController",[ "$scope", "$location", function($scope, media, $location) {
+
+    console.log("tapToStartController loaded");
+
+    var TAPS_FOR_SETTINGS = 5;
+    var TAP_TIMEOUT = 1000;
+
+    var taps = [];
+
+    $scope.upperLeftTap = function() {
+
+    	if (taps.length < TAPS_FOR_SETTINGS) {
+    		taps.push(new Date().getTime());
+    	}
+
+    	else {
+    		var diff = taps[taps.length - 1] - taps[0];
+    		if (diff <= TAP_TIMEOUT) {
+    			$location.path("/setup");
+    		}
+
+    		taps.shift();
+    		taps.push(new Date().getTime());
+    	}
+    }
+}]);
