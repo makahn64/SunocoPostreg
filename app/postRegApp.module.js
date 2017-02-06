eceefd19f21cd9607c991f2ab75e555980d90c4c
Mitch Kahn <mitch@ourglass.io>
2017-02-06 11:39:27 -0800

    Changes to remove skinned backgrounds because $rootScope isn't working like it used to in older Angular versions.

--- app/postRegApp.module.js
+++ app/postRegApp.module.js
@@ -20,36 +20,57 @@ app.run(function($rootScope, $log, $http, $location, Idle, media, a8API, userDef
 
     // defaults in case JSON is messed up
     $rootScope.settings = {
-        "orientation" : "landscape",
-        "aspect" : "16x9",
-        "picsPerPage" : 6,
-        "eventId" : 1,
-        "customerFolder" : "sunoco",
-        "collect" : [ "firstName", "lastName", "email" ]
+        "orientation":           "landscape",
+        "aspect":                "16x9",
+        "picsPerPage":           6,
+        "eventId":               1,
+        "customerFolder":        "sunoco",
+        "pageTitle":             "Sunoco Celebration Cam",
+        "nukeAfterShare":        true,
+        "flagAfterShare":        true,
+        "useExistingExperience": true,
+        "collect":               [ "firstName", "lastName", "email" ],
+        "a8SiteOrigin":          "http://127.0.0.1:1337/api/v2"
+    }
+
+    $rootScope.skin = {
+        customerFolder: $rootScope.settings.customerFolder,
+        title:          $rootScope.settings.pageTitle
     };
 
+    var savedOrigin = userDefaults.getStringForKey( "a8SiteOrigin", "" );
+    if ( savedOrigin ) {
+        $rootScope.settings.a8SiteOrigin = savedOrigin;
+    }
+
+    $log.info( "Setting a8 site origin to " + $rootScope.settings.a8SiteOrigin );
+    a8API.setSiteOrigin( $rootScope.settings.a8SiteOrigin );
+    // do initial media load
+    media.load();
+
+    // MAK took this out. Causing problems with latest ANgular 2017
     // load in settings
-    $http.get("appsettings.json").then(
-        function(data) {
-            $rootScope.settings = data.data;
-            $rootScope.skin = {
-                customerFolder: data.data.customerFolder,
-                title: data.data.pageTitle
-            };
-
-            var savedOrigin = userDefaults.getStringForKey("a8SiteOrigin", "");
-            if (savedOrigin) {
-                $rootScope.settings.a8SiteOrigin = savedOrigin;
-            }
-
-            $log.info("Setting a8 site origin to " + $rootScope.settings.a8SiteOrigin);
-            a8API.setSiteOrigin($rootScope.settings.a8SiteOrigin);
-            // do initial media load
-            media.load();
-        },
-        function() {
-            $log.error("Couldn't load appsettings.json.");
-        });
+    // $http.get("appsettings.json").then(
+    //     function(data) {
+    //         $rootScope.settings = data.data;
+    //         $rootScope.skin = {
+    //             customerFolder: data.data.customerFolder,
+    //             title: data.data.pageTitle
+    //         };
+    //
+    //         var savedOrigin = userDefaults.getStringForKey("a8SiteOrigin", "");
+    //         if (savedOrigin) {
+    //             $rootScope.settings.a8SiteOrigin = savedOrigin;
+    //         }
+    //
+    //         $log.info("Setting a8 site origin to " + $rootScope.settings.a8SiteOrigin);
+    //         a8API.setSiteOrigin($rootScope.settings.a8SiteOrigin);
+    //         // do initial media load
+    //         media.load();
+    //     },
+    //     function() {
+    //         $log.error("Couldn't load appsettings.json.");
+    //     });
 });
 
 
